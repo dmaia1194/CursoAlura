d17ab51b591fce5a5d6946c4b23e35c1b3bd7fdd
Deivid Maia <deivid.sc@gmail.com>
2018-02-14 16:49:20 -0200

    Aula 6 - Organizando melhor nosso código
    Atividade 7: Menu no app.py

--- Python2/app.py
+++ Python2/app.py
@@ -5,3 +5,16 @@ def cadastrar(nomes):
 
 	nome = raw_input()
 	nomes.append(nome)
+
+def menu():
+	nomes = []
+	escolha = ''
+
+	while(escolha!='0'):
+		print 'Digite: 1 para cadastrar, 0 para terminar'
+
+		escolha = raw_input()
+		if(escolha=='1'):
+			cadastrar(nomes)
+
+menu()

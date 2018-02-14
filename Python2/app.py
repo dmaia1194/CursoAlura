e577e4586780196b68a2edf8bfd7a988fc587f67
Deivid Maia <deivid.sc@gmail.com>
2018-02-14 16:53:32 -0200

    Aula 6 - Organizando melhor nosso código
    Atividade 10: Remover um nome

--- Python2/app.py
+++ Python2/app.py
@@ -12,17 +12,25 @@ def listar(nomes):
 	for nome in nomes:
 		print nome
 
+def remover(nomes):
+	print 'Qual nome você gostaria de remover?'
+
+	nome = raw_input()
+	nomes.remove(nome)
+
 def menu():
 	nomes = []
 	escolha = ''
 
 	while(escolha!='0'):
-		print 'Digite: 1 para cadastrar, 2 para listar, 0 para terminar'
+		print 'Digite: 1 para cadastrar, 2 para listar, 3 para remover, 0 para terminar'
 
 		escolha = raw_input()
 		if(escolha=='1'):
 			cadastrar(nomes)
 		if(escolha=='2'):
 			listar(nomes)
+		if(escolha=='3'):
+			remover(nomes)
 
 menu()

e03dadfcc4e147f598edfdb5010a09476449b6f2
Deivid Maia <deivid.sc@gmail.com>
2018-02-14 19:28:42 -0200

    Aula 9 - Encapsulamento
    Atividade 8: A classe Perfil e atributo "privado" no mundo Python

--- Python2/models.py
+++ Python2/models.py
@@ -7,7 +7,13 @@ class Perfil(object):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
-		self.curtidas = 0
+		self.__curtidas = 0
 
 	def imprimir(self):
 		print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+
+	def curtir(self):
+		self.__curtidas += 1
+
+	def devolve_curtidas(self):
+		return self.__curtidas

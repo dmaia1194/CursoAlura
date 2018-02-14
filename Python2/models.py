df0a39f0875358cf7671718a0b9ff63c9381761d
Deivid Maia <deivid.sc@gmail.com>
2018-02-14 20:15:05 -0200

    Aula 12 - Métodos estáticos

--- Python2/models.py
+++ Python2/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 class Perfil(object):
-	'Classe para moldar perfis de usuarios'
+	'Classe padrão para perfis de usuarios'
 
 	def __init__(self, nome, telefone, empresa):
 		self.nome = nome
@@ -18,8 +18,25 @@ class Perfil(object):
 	def devolve_curtidas(self):
 		return self.__curtidas
 
+	@classmethod
+	def gerar_perfis(classe, nome_arquivo):
+		arquivo = open(nome_arquivo, 'r')
+		perfis = []
+
+		for linha in arquivo:
+			valores = linha.split(',')
+			perfis.append(classe(*valores))
+
+		arquivo.close()
+
+		return perfis
+
 class Perfil_Vip(Perfil):
 	'Classe padrão para perfis de usuários VIPs'
+
+	def __init__(self, nome, telefone, empresa, apelido = ''):
+		super(Perfil_Vip, self).__init__(nome, telefone, empresa)
+		self.apelido = apelido
 
 	def devolve_creditos(self):
 		return super(Perfil_Vip, self).devolve_curtidas() * 10.0

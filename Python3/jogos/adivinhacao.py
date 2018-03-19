078f83018507382fce622de5d9827306b54b2632
Deivid Maia <deivid.sc@gmail.com>
2018-03-19 10:18:22 -0300

    Aula 3 - Testando valores
    Atividade 5: Dando dicas

--- Python3/jogos/adivinhacao.py
+++ Python3/jogos/adivinhacao.py
@@ -8,9 +8,17 @@ chute_str = input("Digite o seu número: ")
 print("Você digitou: ", chute_str)
 chute = int(chute_str)
 
-if(numero_secreto == chute):
+acertou = chute == numero_secreto
+maior	= chute == numero_secreto
+menor	= chute == numero_secreto
+
+if(acertou):
 	print("Você acertou!")
 else:
 	print("Você errou!")
+	if(maior):
+		print("O seu chute foi maior que o número secreto!")
+	elif(menor):
+		print("O seu chute foi menor que o número secreto!")
 
 print("Fim de jogo")

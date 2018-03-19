16c931b68e0f65e8e35369e3324cc218fc2e489a
Deivid Maia <deivid.sc@gmail.com>
2018-03-19 10:41:17 -0300

    Ignorar perfis.csv

--- Python3/jogos/adivinhacao.py
+++ Python3/jogos/adivinhacao.py
@@ -3,22 +3,29 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+while(rodada <= total_tentativas):
+	print("Tentativa {} de {}".format(rodada, total_tentativas))
 
-acertou = chute == numero_secreto
-maior	= chute == numero_secreto
-menor	= chute == numero_secreto
+	chute_str = input("Digite o seu número: ")
+	print("Você digitou: ", chute_str)
+	chute = int(chute_str)
 
-if(acertou):
-	print("Você acertou!")
-else:
-	print("Você errou!")
-	if(maior):
-		print("O seu chute foi maior que o número secreto!")
-	elif(menor):
-		print("O seu chute foi menor que o número secreto!")
+	acertou = chute == numero_secreto
+	maior	= chute == numero_secreto
+	menor	= chute == numero_secreto
+
+	if(acertou):
+		print("Você acertou!")
+	else:
+		print("Você errou!")
+		if(maior):
+			print("O seu chute foi maior que o número secreto!")
+		elif(menor):
+			print("O seu chute foi menor que o número secreto!")
+
+	rodada = rodada + 1
 
 print("Fim de jogo")
